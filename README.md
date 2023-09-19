b8f4d365332fbf74b59582a7e3b6f33e361ea6c8
Ibi Keller <ibi.keller@gmail.com>
2023-09-18 21:21:31 -0400

    0.0.1: Basic file structure implemented

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flac2mp3
+
+A C# tool to convert files from .flac to .mp3 using ffmpeg, then remove the .flac files
---
+++ flac2mp3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace flac2mp3 {
+
+    internal class Program {
+
+        private static void Main(string[] args) {
+            if (args.Length < 1) {
+                Console.WriteLine("No path specified. Exiting");
+                return;
+            }
+
+            string rootPath = args[0];
+            string cmdFfmpeg = "ffmpeg";
+
+            Recurse(new DirectoryInfo(rootPath));
+        }
+
+        private static void Recurse(DirectoryInfo dir) {
+            foreach (var file in dir.EnumerateFiles().Where(f => f.Extension.ToLower() == "flac"))
+                ProcessFLAC(file);
+
+            foreach (var sub in dir.EnumerateDirectories())
+                Recurse(sub);
+        }
+
+        private static void ProcessFLAC(FileInfo flac) {
+            Console.WriteLine(flac.FullName);
+        }
+    }
+}
