627d3c362d3f8fcabea4d0f94baefb5d19b3de4b
Ibi Keller <ibi.keller@gmail.com>
2023-09-18 22:03:02 -0400

    0.0.3: Parallel tasking of conversions; Correct ffmpeg args used

--- flac2mp3.cs
+++ flac2mp3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace flac2mp3 {
 
@@ -17,21 +18,21 @@ namespace flac2mp3 {
         }
 
         private static void Recurse(DirectoryInfo dir) {
-            foreach (var file in dir.EnumerateFiles().Where(f => f.Extension.ToLower() == ".flac"))
+            Parallel.ForEach(dir.EnumerateFiles().Where(f => f.Extension.ToLower() == ".flac"), file => {
                 ProcessFLAC(file);
+            });
 
-            foreach (var sub in dir.EnumerateDirectories())
+            Parallel.ForEach(dir.EnumerateDirectories(), sub => {
                 Recurse(sub);
+            });
         }
 
         private static void ProcessFLAC(FileInfo flac) {
             string mp3 = Path.ChangeExtension(flac.FullName, ".mp3");
-            Console.WriteLine($"{flac.FullName} --> {mp3}");
-
             Process proc = new Process();
 
             proc.StartInfo.FileName = "ffmpeg";
-            proc.StartInfo.Arguments = $"-i \"{flac.FullName}\" -ab 320k -map_metadata 0 -id3v2_version 3 \"mp3\"";
+            proc.StartInfo.Arguments = $"-i \"{flac.FullName}\" -vn -c:a libmp3lame -ab 320k -y \"{mp3}\"";
 
             proc.Start();
             proc.WaitForExit();
